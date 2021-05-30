179e7f2c0eb71746f7eb537d1689dc01c808a4f9
vivek715 <62061464+vivek715@users.noreply.github.com>
2021-05-30 20:07:23 +0530

    not required done for added for testing purposes

--- database/even_scheduler_delete.sql
+++
@@ -1,8 +0,0 @@
-SET GLOBAL event_scheduler = ON;
-
-create event cleaning 
-on schedule every 2 minute
-starts current_timestamp()
-do
-	delete from shortener.url
-    where url.expires_date < now();
